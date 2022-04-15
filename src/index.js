a197adb715ea4fa08c8df8a207f1b8c15e8008ac
oldbee705 <577834917@qq.com>
2022-04-15 11:26:41 +0800

    4.15

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import EF from './EmailForm';
+import NL from './NameList';
 
 
 ReactDOM.render(
-  <EF />,
+  <NL />,
   document.getElementById('root')
 );
