64332156a8a42529a0505099087fccfaacb79710
oldbee705 <577834917@qq.com>
2022-04-20 10:03:34 +0800

    4.20

--- src/NameList/index.js
+++ src/NameList/index.js
@@ -1,32 +1,70 @@
-import { useState } from 'react';
+import { useImmer } from 'use-immer';
 
-let nextId = 0;
+const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+];
 
-function NameList() {
-  const [name, setName] = useState('');
-  const [artists, setArtists] = useState([]);
+function BucketList() {
+  const [myList, updateMyList] = useImmer(initialList);
+  const [yourList, updateYourList] = useImmer(initialList);
+
+  function handleToggleMyList(artworkId, nextSeen) {
+    updateMyList(draft => {
+      const artwork = draft.find(a =>
+        a.id === artworkId
+        );
+        artwork.seen = nextSeen;
+    });
+  };
+
+  function handleToggleYourList(artworkId, nextSeen) {
+    updateYourList(draft => {
+      const artwork = draft.find(a =>
+        a.id === artworkId
+        );
+        artwork.seen = nextSeen;
+    });
+  };
 
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      <input
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <button onClick={() => {
-        setName('');
-        setArtists([
-          ...artists,
-          { id: nextId++, name: name }
-        ]);
-      }}>Add</button>
-      <ul>
-        {artists.map(artist => (
-          <li key={artist.id}>{artist.name}</li>
-        ))}
-      </ul>
+      <h1>Art Bucket List</h1>
+      <h2>My list of art to see:</h2>
+      <ItemList
+        artworks={myList}
+        onToggle={handleToggleMyList} />
+      <h2>Your list of art to see:</h2>
+      <ItemList
+        artworks={yourList}
+        onToggle={handleToggleYourList} />
     </>
   );
 }
 
-export default NameList;
+function ItemList({ artworks, onToggle }) {
+  return (
+    <ul>
+      {artworks.map(artwork => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={e => {
+                onToggle(
+                  artwork.id,
+                  e.target.checked
+                );
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default BucketList;
